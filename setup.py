9d63cfce5606bb2376847c2dec691d5aab23e8ad
Youguang-Zhou <youguang.zhou@outlook.com>
2022-04-08 01:55:11 +0100

    RELEASE 1.0.3

--- setup.py
+++ setup.py
@@ -5,11 +5,12 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setuptools.setup(
     name="bilibili-dl",
-    version="1.0.2",
+    version="1.0.3",
     description="Bilibili-dl 是一个下载 B 站视频的工具（目前只支持下载音频）",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Youguang-Zhou/bilibili-dl",
+    packages=setuptools.find_packages(),
     entry_points={
         'console_scripts': ['bilibili-dl=bilibili_dl.cli:run_cli']
     },
